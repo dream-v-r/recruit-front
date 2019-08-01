214b4d4ef0486a6b611ac24e67670f2aecb3b593
kango furusu <furusu0715@gmail.com>
2019-08-02 01:40:18 +0900

    セクション２ before調整

--- components/Top/SectionMain02.vue
+++ components/Top/SectionMain02.vue
@@ -3,7 +3,6 @@
     <div class="inner">
       <div class="section-block block">
         <div class="section-inner">
-          <div class="bg"></div>
           <div class="img01">
             <img
               src="~/assets/img/top/section02_img01.jpg"
@@ -16,20 +15,20 @@
               alt=""
             >
           </div>
-        </div>
-        <div class="feature section-feature">
-          <SectionMainTitle
-            number="02"
-            title="Culture"
-            comment="社員の笑顔の源。社員の声から生まれる"
-            class="title"
-          />
-          <SectionMainFeature
-            title="福利厚生・社風"
-            detail="大手IT企業にも負けない様々な福利厚生や<br>社員サポートを行っております。"
-            url="culture"
-            class="content"
-          />
+          <div class="feature section-feature">
+            <SectionMainTitle
+              number="02"
+              title="Culture"
+              comment="社員の笑顔の源。社員の声から生まれる"
+              class="title"
+            />
+            <SectionMainFeature
+              title="福利厚生・社風"
+              detail="大手IT企業にも負けない様々な福利厚生や<br>社員サポートを行っております。"
+              url="culture"
+              class="content"
+            />
+          </div>
         </div>
       </div>
     </div>
@@ -50,15 +49,14 @@
   .main-section {
     position: relative;
     > .inner {
-      max-width: 1596px;
-      margin-left: auto;
+      max-width: 1366px;
+      margin: auto;
       > .block {
       }
     }
   }
 
   .section-block {
-    padding-left: 20px;
     background-image: url("~assets/img/top/section02_bg_text.png");
     background-position: left 20px bottom 183px;
     padding-bottom: 121px;
@@ -67,9 +65,9 @@
       background-position: left 0 bottom 36px;
       padding-bottom: 71px;
     }
-    > .feature {
+    .feature {
       position: absolute;
-      top: -43px;
+      top: -106px;
       left: 51.61054%;
       max-width: 40.42690%;
       @include _mqmax(){
@@ -87,20 +85,21 @@
 
   .section-inner {
     position: relative;
-    > .bg {
+    &::before {
+      content: '';
+      position: absolute;
       background-image: url("~assets/img/top/section_bg.jpg");
       background-repeat: repeat;
-      padding-top: 35.339%;
-      width: calc(100% - 230px);
-      margin-left: auto;
-      @include _mqmax(){
-        width: calc(100% - 95px);
-        padding-top: 77.18309%;
-      }
+      z-index: -1;
+      width: calc(83.089311% + 50vw);
+      padding-top: 41.72767%;
+      top: -60px;
+      left: 0;
+      margin-left: 16.398243%;
     }
     > .img01 {
-      position: absolute;
-      max-width: 40.10025%;
+      /*position: absolute;*/
+      max-width: 46.852122%;
       top: 67px;
       @include _mqmax(){
         max-width: 84.50704%;
@@ -108,9 +107,9 @@
       }
     }
     > .img02 {
-      margin-top: -47px;
+      margin-top: 48px;
       margin-left: 21.22986%;
-      max-width: 18.9223%;
+      max-width: 22.1083455%;
       @include _mqmax(){
         position: relative;
         max-width: 31.40845%;
