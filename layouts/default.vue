b1f5236409d5efa0b3f9a6df52efc60f108f2562
kango furusu <furusu0715@gmail.com>
2019-08-12 02:32:00 +0900

    スクロール時　ヘッダーfixed

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,35 +1,36 @@
 <template>
   <div v-scroll="handleScroll">
-    <TheHeader :class="{isFixed: isFixed}" />
-    <nuxt />
-    <TheEntry />
-    <TheFooter />
+    <TheHeader :class="{isFixed: isFixed}"/>
+    <nuxt/>
+    <TheEntry/>
+    <TheFooter/>
   </div>
 </template>
 
 <script>
-import TheHeader from '../components/layouts/TheHeader'
-import TheFooter from '../components/layouts/TheFooter'
-import TheEntry from '../components/layouts/TheEntry'
-export default {
-  components: { TheEntry, TheFooter, TheHeader },
-  data() {
-    return {
-      offsetY: 0,
-      isFixed: false
-    }
-  },
-  mounted() {
-    this.offsetY = 969;
-  },
-  methods: {
-    handleScroll: function() {
-      if(this.offsetY < window.scrollY) {
-        this.isFixed = true
-      } else {
-        this.isFixed = false
+  import TheHeader from '../components/layouts/TheHeader'
+  import TheFooter from '../components/layouts/TheFooter'
+  import TheEntry from '../components/layouts/TheEntry'
+
+  export default {
+    components: { TheEntry, TheFooter, TheHeader },
+    data() {
+      return {
+        offsetY: 0,
+        isFixed: false
+      }
+    },
+    mounted() {
+      this.offsetY = 300
+    },
+    methods: {
+      handleScroll: function() {
+        if (this.offsetY < window.scrollY) {
+          this.isFixed = true
+        } else {
+          this.isFixed = false
+        }
       }
     }
   }
-}
 </script>
