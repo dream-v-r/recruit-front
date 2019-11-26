09df7e37bf1ae2e83599ca2cc14b72cb926620ec
rienagamatsu <57780069+rienagamatsu@users.noreply.github.com>
2019-11-26 13:11:38 +0900

    ヘッダーナビゲーションのエントリーボタンを「新卒採用」に外部リンクマーク設置

--- components/App/AppEntry.vue
+++ components/App/AppEntry.vue
@@ -25,19 +25,64 @@ export default {
   justify-content: center;
   margin-left: auto;
   font-weight: bold;
-  @include _mqmax() {
-    font-size: 10px;
-	margin-left: 10px;
+
+  .title {
+    &::before, &::after {
+      content: '';
+      position: absolute;
+    }
+
+    &:before {
+      background-color: #fff;
+      width: 8px;
+      height: 8px;
+      top: 20px;
+      bottom: 0;
+      margin: auto;
+      right: 138px;
+    }
+
+    &::after {
+      width: 8px;
+      height: 8px;
+      border-top: 1px solid #fff;
+      border-right: 1px solid #fff;
+      top: 15px;
+      bottom: 0;
+      margin: auto;
+      right: 136px;
+    }
+
+    .title2 {
+      display: block;
+      font-size: 13px;
+      font-weight: normal;
+    }
   }
-  > .title {
-	> .title2{
-	display:block;
-	font-size:13px;
-	font-weight: normal;
-	@include _mqmax() {
-		font-size: 10px;
-	}
-	}
+}
+
+@include _mqmax() {
+  .entry-link {
+    font-size: 10px;
+    margin-left: 10px;
+  }
+}
+
+@include _mqmax() {
+  .entry-link .title:before {
+    top: -33px;
+  }
+}
+
+@include _mqmax() {
+  .entry-link .title::after {
+    top: -38px;
+  }
+}
+
+@include _mqmax() {
+  .entry-link .title .title2 {
+    font-size: 10px;
   }
 }
 </style>
