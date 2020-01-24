d163c6240682baf80e7582f3f6e7b8ae7c094f39
rienagamatsu <57780069+rienagamatsu@users.noreply.github.com>
2020-01-24 15:16:01 +0900

    サイトマップページ作成

---
+++ components/Sitemap/Section01.vue
@@ -0,0 +1,186 @@
+<template>
+  <section class="section02">
+    <div class="sec-contents" id="company">
+      <dl class="sec-def">
+        <dt><a href="/">HOME<span>ホーム</span></a></dt>
+        <dd></dd>
+        <dt><a href="/message/">MESSAGE<span>メッセージ</span></a></dt>
+        <dd></dd>
+        <dt><a href="/people_index/">MEMBER<span>社員紹介</span></a></dt>
+        <dd>
+          <ul class="list">
+            <li><a href="/people_vol1/">社員紹介Vol.1</a></li>
+            <li><a href="/people_vol2/">社員紹介Vol.2</a></li>
+            <li><a href="/people_vol3/">社員紹介Vol.3</a></li>
+            <li><a href="/people_vol4/">社員紹介Vol.4</a></li>
+            <li><a href="/people_vol5/">社員紹介Vol.5</a></li>
+            <li><a href="/people_vol6/">社員紹介Vol.6</a></li>
+            <li><a href="/people_vol7/">社員紹介Vol.7</a></li>
+          </ul>
+        </dd>
+        <dt><a href="/culture-company-style/">CULTURE<span>福利厚生・社風</span></a></dt>
+        <dd>
+          <ul class="list">
+            <li><a href="/culture-company-style/">社員アンケートからひも解く社風</a></li>
+            <li><a href="/culture-welfare/">福利厚生</a></li>
+          </ul>
+        </dd>
+        <dt><a href="/about/">COMPANY<span>会社概要</span></a></dt>
+        <dd></dd>
+        <dt><a href="/job/career/">RECRUIT<span>募集要項</span></a></dt>
+        <dd>
+          <ul class="list">
+            <li><a href="/job/newgrads/">新卒採用</a></li>
+            <li><a href="/job/career/">キャリア採用</a></li>
+          </ul>
+        </dd>
+        <dt><a href="/seminar/">SEMINAR<span>説明会</span></a></dt>
+        <dd></dd>
+        <dt><a href="https://rikunabi-direct.jp/2020/corporation/detail/rd00036185" target="_blank">NEW GRADUATE ENTRY
+        <img
+          src="~/assets/img/sitemap/externallink.png"
+          alt="別タブで外部サイトが開きます"
+        >
+        <span>新卒採用エントリー</span></a>
+        </dt>
+        <dd></dd>
+        <dt><a href="/career_entry/">CAREER ENTRY<span>キャリア採用エントリー</span></a></dt>
+        <dd></dd>
+        <dt><a href="https://www.dream-v.co.jp/" target="_blank">CORPORATE SITE
+        <img
+          src="~/assets/img/sitemap/externallink.png"
+          alt="別タブでページが開きます"
+        >
+        <span>コーポレートサイト</span></a></dt>
+        <dd></dd>
+        <dt><a href="https://www.dream-v.co.jp/privacy/" target="_blank">PRIVACY POLICY
+        <img
+          src="~/assets/img/sitemap/externallink.png"
+          alt="別タブでページが開きます"
+        >
+        <span>プライバシーポリシー</span></a></dt>
+        <dd></dd>
+      </dl>
+    </div>
+  </section>
+</template>
+
+<style scoped lang="scss">
+  .section02 {
+    color: #333;
+    line-height: 1;
+    padding-top: 80px;
+    @include _mqmax(){
+      padding-top: 0;
+    }
+    .sec-contents {
+      max-width: 1000px;
+      margin: -94px auto 0;
+      padding: 94px 0 48px;
+      @include _mqmax(){
+        max-width: none;
+        margin: 0 auto 0;
+        padding: 30px 0 20px;
+      }
+      .title {
+        color: #fff;
+        font-size: 20px;
+        font-weight: bold;
+        text-align: center;
+        background-color: #029ae4;
+        padding: 20px 0;
+        margin-bottom: 60px;
+        @include _mqmax(){
+          font-size: 25px;
+          padding: 22px 0 23px;
+          margin-bottom: 39px;
+        }
+      }
+    }
+  }
+  .sec-def {
+    display: flex;
+    flex-wrap: wrap;
+    font-size: 14px;
+    @include _mqmax(){
+      font-size: 15px;
+      padding: 0 20px;
+    }
+    dt {
+      width: 300px;
+      color: #029AE4;
+      font-weight: bold;
+      border-top: 2px solid #029AE4;
+      padding: 22px 0 22px 10px;
+      @include _mqmax(){
+        width: 100%;
+        border-top: 1px solid #029AE4;
+        padding: 20px 0 0 5px;
+      }
+    }
+    dd {
+      width: calc(100% - 300px);
+      color: #029AE4;
+      line-height: 1;
+      font-weight: bold;
+      border-top: 2px solid #029AE4;
+      padding: 10px 0 22px 10px;
+      @include _mqmax(){
+        width: 100%;
+        border-top: none;
+        padding: 0 0 10px;
+        margin-left: 0;
+      }
+    }
+    .list {
+      li {
+        padding: 1em 0 1em 1em;
+        position: relative;
+      }
+        @include _mqmax(){
+          padding-left: 0;
+       }
+    }
+  }
+
+
+
+  dt a, dd a,{
+    padding-left: 2em;
+    position:relative;
+    text-decoration: none;
+    color: #029AE4;
+  }
+    dt a:hover, dd a:hover,{
+    color: #f58a00;
+  }
+  dt a:before, dd a:before,{
+      content: '';
+      width: 6px;
+      height: 6px;
+      display: inline-block;
+      border-top: 1px solid #2383c5;
+      border-right: 1px solid #2383c5;
+      -webkit-transform: rotate(45deg);
+      transform: rotate(45deg);
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      margin: auto;
+  }
+  dt a img, dd a img{
+    padding: 3px;
+  }
+  dt a span, dd a span,{
+    display: block;
+    font-weight: normal;
+    color: #555;
+    font-size: 12px;
+    line-height: 1.4;
+    padding-left: 28px;
+    @include _mqmax(){
+      padding-left: 30px;
+    }
+  }
+</style>
---
+++ pages/sitemap.vue
@@ -0,0 +1,54 @@
+<template>
+  <main>
+    <KeyVisual class="sitemap" />
+    <Section01 class="sitemap" />
+  </main>
+</template>
+
+<script>
+  import Meta from '~/assets/mixins/meta'
+  import KeyVisual from '../components/sitemap/KeyVisual'
+  import Section01 from '../components/sitemap/Section01'
+
+  export default {
+    components: { KeyVisual, Section01 },
+    mixins: [Meta],
+    data () {
+      return {
+        meta: {
+          title: '会社概要',
+          description: 'ドリームビジョン株式会社の会社概要をご紹介しております。ドリームビジョンは日本一エンジニアにやさしい企業を目指しております。',
+          keywords: '採用情報,エンジニア,クリエイティブ,Web,IT,システム開発,ネットワーク,インフラ,小伝馬町,ドリームビジョン株式会社,新卒採用,中途採用,会社概要',
+          url: 'message'
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  p {
+    color: $mainColor;
+  }
+  .top-main {
+    > .section {
+      margin-top: 120px;
+      border-bottom: 1px solid #999;
+      @include _mqmax(){
+        margin-top: 50px;
+      }
+    }
+    > .banner {
+      border-bottom: 1px solid #999;
+    }
+    > .news {
+      margin-top: -65px;
+      position: relative;
+      z-index: 1;
+      @include _mqmax() {
+        padding-left: 20px;
+        padding-right: 20px;
+      }
+    }
+  }
+</style>
